47bf18d2625c2b3759a2ce363bed8f80bf539fb5
niikpakpobrown <brown.niikpakpo@gmail.com>
2022-06-21 22:47:42 +0000

    Tasks 0 and 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """This represents a square.
+    Private instance attribute: size.
+    Instantiation with size (no type/value verification).
+    """
+
+    def __init__(self, size):
+        """Initialization of the data."""
+        self.__size = size
